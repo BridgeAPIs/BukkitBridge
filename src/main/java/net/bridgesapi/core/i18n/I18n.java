5e3aec0c0980146d8a89139b4321a0d4fac38dbf
zyuiop <zyuiop@zyuiop.net>
2015-05-26 18:33:14 +0200

    Updated I18n load module

--- src/main/java/net/bridgesapi/core/i18n/I18n.java
+++ src/main/java/net/bridgesapi/core/i18n/I18n.java
@@ -1,32 +1,34 @@
 package net.bridgesapi.core.i18n;
 
-import com.google.common.io.ByteStreams;
 import net.bridgesapi.core.APIPlugin;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.*;
+import java.nio.file.Files;
 
 public class I18n {
 
     private static Configuration lang;
 
-    public static void load(String lang, APIPlugin plugin) throws IOException {
-        File file = new File(plugin.getDataFolder().getPath() + "/i18n/" + lang + ".lang");
-        if (!file.exists()) {
-            file = new File(plugin.getDataFolder().getPath() + "/i18n/default.lang");
-            if (!file.exists()) {
-                new File(plugin.getDataFolder().getPath() + "/i18n/").mkdir();
-                file.createNewFile();
-                try (InputStream is = plugin.getResource("default.lang");
-                     OutputStream os = new FileOutputStream(file)) {
-                     ByteStreams.copy(is, os);
-                }
-            }
-        }
+	public static void load(String lang, APIPlugin plugin) throws IOException {
+		File file = new File(plugin.getDataFolder().getPath() + "/i18n/" + lang + ".lang");
+		if (!file.exists()) {
+			InputStream stream = plugin.getResource(lang + ".lang");
+			if (stream == null) {
+				stream = plugin.getResource("default.lang");
+				lang = "default";
+			}
+
+			file = new File(plugin.getDataFolder().getPath() + "/i18n/" + lang + ".lang");
+			if (!new File(plugin.getDataFolder().getPath() + "/i18n/").exists())
+				new File(plugin.getDataFolder().getPath() + "/i18n/").mkdir();
+
+			Files.copy(stream, file.toPath());
+		}
 
 		I18n.lang = YamlConfiguration.loadConfiguration(file);
-    }
+	}
 
     public static String getTranslation(String key) {
         if (lang == null)
